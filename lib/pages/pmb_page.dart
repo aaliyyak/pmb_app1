be70b6cf0e8aa025541fe3ae3e26beab6ea3cc86
aaliyyak <sayu38834@gmail.com>
2024-08-07 22:07:22 +0800

    update

--- lib/pages/pmb_page.dart
+++ lib/pages/pmb_page.dart
@@ -1,112 +1,277 @@
 import 'package:flutter/material.dart';
+import 'package:pmb_app/themes/themes.dart';
+import 'package:pmb_app/pages/prodi_page.dart'; // Pastikan import halaman prodi_page
 
 class PmbPage extends StatefulWidget {
   const PmbPage({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _PmbPageState createState() => _PmbPageState();
 }
 
 class _PmbPageState extends State<PmbPage> {
+  bool isSarjanaSelected = true;
+
+  Widget _buildContentBox(String title, String description, Color color,
+      String imageAsset, String programLabel) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      margin: EdgeInsets.symmetric(vertical: 3, horizontal: 4),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 8,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 2,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              child: Text(
+                programLabel,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 30), // Add spacing to avoid overlap
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 2),
+                    Text(
+                      description,
+                      style: TextStyle(fontSize: 14, color: Colors.white),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProdiPage(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: primaryColor, // Button color
+                        padding: EdgeInsets.symmetric(
+                            vertical: 1, horizontal: 15), // Adjust padding
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(20), // Button radius
+                        ),
+                      ),
+                      child: Text(
+                        'Lihat Program Studi',
+                        style: TextStyle(
+                          fontSize: 10, // Smaller text size
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold, // Text color
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 120, // Fixed width to maintain box size
+                height: 120, // Fixed height to maintain box size
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    imageAsset,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildToggleButton(String title, bool isSelected, VoidCallback onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 15),
+        decoration: BoxDecoration(
+          color: isSelected ? primaryColor : Colors.grey[300],
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: isSelected
+              ? [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 8,
+                    offset: Offset(0, 4),
+                  ),
+                ]
+              : [],
+        ),
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              color: isSelected ? Colors.white : Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffd9d9d9),
+      backgroundColor: Colors.grey[50],
       appBar: AppBar(
-        backgroundColor: Color(0xff0d3559),
+        title: Text(
+          'Penerimaan Mahasiswa Baru',
+          style: TextStyle(fontSize: 18, color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
         elevation: 0,
         centerTitle: true,
-        toolbarHeight: 35,
+        toolbarHeight: 50,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
+          icon: Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () {
             Navigator.of(context).pop();
           },
         ),
-        shape: ContinuousRectangleBorder(
-          borderRadius: BorderRadius.only(
-            bottomRight: Radius.circular(0),
-          ),
-        ),
+        //actions: [
+        // IconButton(
+        //  icon: Icon(Icons.search, color: Colors.black),
+        //  onPressed: () {
+        // Add search functionality here
+        // },
       ),
+      // ],
+      //  ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Color(0xff0d3559),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.elliptical(95, 150),
-                    bottomRight: Radius.elliptical(600, 250),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: _buildToggleButton(
+                          'Sarjana (S1)', isSarjanaSelected, () {
+                        setState(() {
+                          isSarjanaSelected = true;
+                        });
+                      }),
+                    ),
+                    SizedBox(width: 1), // Reduced space between buttons
+                    Expanded(
+                      child: _buildToggleButton(
+                          'Pascasarjana', !isSarjanaSelected, () {
+                        setState(() {
+                          isSarjanaSelected = false;
+                        });
+                      }),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                if (isSarjanaSelected) ...[
+                  _buildContentBox(
+                    'Ilmu Komputer',
+                    'Fakultas Teknik',
+                    Colors.purple,
+                    'assets/qwbb.jpg',
+                    'S1',
                   ),
-                ),
-                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 20),
-                    Row(
-                      children: [
-                        Text(
-                          "Riwayat Pendaftaran",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Spacer(),
-                      ],
-                    ),
-                    SizedBox(height: 25),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Cari Informasi Riwayat',
-                        hintStyle: TextStyle(fontSize: 12),
-                        filled: true,
-                        fillColor: Colors.white,
-                        prefixIcon: Icon(Icons.search, color: Colors.grey),
-                        contentPadding: EdgeInsets.symmetric(
-                          vertical: 1,
-                          horizontal: 0.5,
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 10),
-                    // Your content for Riwayat Page
-                    // For example, a list of history items
-                    Card(
-                      child: ListTile(
-                        title: Text('Pendaftaran 1'),
-                        subtitle: Text('Tanggal: 20 Juni 2023'),
-                      ),
-                    ),
-                    Card(
-                      child: ListTile(
-                        title: Text('Pendaftaran 2'),
-                        subtitle: Text('Tanggal: 22 Juni 2023'),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+                  _buildContentBox(
+                    'Matematika',
+                    'Fakultas MIPA',
+                    Colors.teal,
+                    'assets/qww.jpg',
+                    'S1',
+                  ),
+                  _buildContentBox(
+                    'Akuntansi',
+                    'Fakultas Ekonomi',
+                    Colors.orange,
+                    'assets/qwr.jpg',
+                    'S1',
+                  ),
+                  _buildContentBox(
+                    'Psikologi',
+                    'Fakultas Psikologi',
+                    Colors.blue,
+                    'assets/ayor.jpg',
+                    'S1',
+                  ),
+                  _buildContentBox(
+                    'Teknologi Hasil Pertanian',
+                    'Fakultas Pertanian',
+                    Colors.red,
+                    'assets/qwb.jpg',
+                    'S1',
+                  ),
+                  _buildContentBox(
+                    'mencintai dia',
+                    'Fakultas cinta',
+                    secondaryColor,
+                    'assets/qwbb.jpg',
+                    'S1',
+                  ),
+                ] else ...[
+                  _buildContentBox(
+                    'Magister Manajemen',
+                    'Fakultas Ekonomi',
+                    Colors.purple,
+                    'assets/5r.jpg',
+                    'S2',
+                  ),
+                  _buildContentBox(
+                    'Magister Teknik Informatika',
+                    'Fakultas Teknik',
+                    Colors.teal,
+                    'assets/5.jpg',
+                    'S2',
+                  ),
+                ],
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/pages/prodi_page.dart
@@ -0,0 +1,273 @@
+import 'package:flutter/material.dart';
+import 'package:pmb_app/themes/themes.dart';
+
+class ProdiPage extends StatefulWidget {
+  const ProdiPage({super.key});
+
+  @override
+  _ProdiPageState createState() => _ProdiPageState();
+}
+
+class _ProdiPageState extends State<ProdiPage> {
+  Widget _buildContentBox(String title, String description, Color color,
+      String imageAsset, String programLabel) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      margin: EdgeInsets.symmetric(vertical: 3, horizontal: 5),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 8,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 2,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              child: Text(
+                programLabel,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 30), // Add spacing to avoid overlap
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 2),
+                    Text(
+                      description,
+                      style: TextStyle(fontSize: 14, color: Colors.white),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: primaryColor, // Button color
+                        padding: EdgeInsets.symmetric(
+                            vertical: 1, horizontal: 15), // Adjust padding
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(20), // Button radius
+                        ),
+                      ),
+                      child: Text(
+                        'Lihat Detail Program Studi',
+                        style: TextStyle(
+                          fontSize: 10, // Smaller text size
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold, // Text color
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 120, // Fixed width to maintain box size
+                height: 120, // Fixed height to maintain box size
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    imageAsset,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        toolbarHeight: 60,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.grey),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Container(
+          height: 40,
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: 'cari prodi berdasarkan minatmu....',
+              hintStyle: TextStyle(
+                color: Colors.grey,
+                fontSize: 14,
+              ), // Set hint text color
+              filled: true,
+              fillColor: Colors.grey[200],
+              contentPadding: EdgeInsets.all(15),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide.none,
+              ),
+              suffixIcon: Icon(Icons.search, color: Colors.grey),
+            ),
+            onTap: () {
+              // Behavior when the search box is tapped
+            },
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20),
+                _buildContentBox(
+                  'Ilmu Komputer',
+                  'Fakultas Teknik',
+                  Colors.purple,
+                  'assets/qwbb.jpg',
+                  'S1',
+                ),
+                _buildContentBox(
+                  'Matematika',
+                  'Fakultas MIPA',
+                  Colors.teal,
+                  'assets/qww.jpg',
+                  'S1',
+                ),
+                _buildContentBox(
+                  'Akuntansi',
+                  'Fakultas Ekonomi',
+                  Colors.orange,
+                  'assets/qwr.jpg',
+                  'S1',
+                ),
+                _buildContentBox(
+                  'Psikologi',
+                  'Fakultas Psikologi',
+                  Colors.blue,
+                  'assets/ayor.jpg',
+                  'S1',
+                ),
+                _buildContentBox(
+                  'Teknologi Hasil Pertanian',
+                  'Fakultas Pertanian',
+                  Colors.red,
+                  'assets/qwb.jpg',
+                  'S1',
+                ),
+                _buildContentBox(
+                  'mencintai dia',
+                  'Fakultas cinta',
+                  secondaryColor,
+                  'assets/qwbb.jpg',
+                  'S1',
+                ),
+                _buildContentBox(
+                  'Magister Manajemen',
+                  'Fakultas Ekonomi',
+                  Colors.purple,
+                  'assets/5r.jpg',
+                  'S2',
+                ),
+                _buildContentBox(
+                  'Magister Teknik Informatika',
+                  'Fakultas Teknik',
+                  Colors.teal,
+                  'assets/5.jpg',
+                  'S2',
+                ),
+                _buildContentBox(
+                  'Matematika',
+                  'Fakultas MIPA',
+                  Colors.teal,
+                  'assets/qww.jpg',
+                  'S1',
+                ),
+                _buildContentBox(
+                  'Akuntansi',
+                  'Fakultas Ekonomi',
+                  Colors.orange,
+                  'assets/qwr.jpg',
+                  'S1',
+                ),
+                _buildContentBox(
+                  'Psikologi',
+                  'Fakultas Psikologi',
+                  Colors.blue,
+                  'assets/ayor.jpg',
+                  'S1',
+                ),
+                _buildContentBox(
+                  'Teknologi Hasil Pertanian',
+                  'Fakultas Pertanian',
+                  Colors.red,
+                  'assets/qwb.jpg',
+                  'S1',
+                ),
+                _buildContentBox(
+                  'mencintai dia',
+                  'Fakultas cinta',
+                  secondaryColor,
+                  'assets/qwbb.jpg',
+                  'S1',
+                ),
+                _buildContentBox(
+                  'Magister Manajemen',
+                  'Fakultas Ekonomi',
+                  Colors.purple,
+                  'assets/5r.jpg',
+                  'S2',
+                ),
+                _buildContentBox(
+                  'Magister Teknik Informatika',
+                  'Fakultas Teknik',
+                  Colors.teal,
+                  'assets/5.jpg',
+                  'S2',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
